93bbaa5ce863f5bf2b24f79baf1c4be552b67ce5
vgont <vgontijo34@gmail.com>
2023-11-23 19:02:14 -0300

    Sonos page created

---
+++ src/app/sonos/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import LogoHT from "../components/LogoHT";
+
+export default function SleepStress() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <LogoHT />
+    </main>
+  );
+}
