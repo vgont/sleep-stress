4495e19df24ea5a483addfb8be6b609ff455bcae
vgont <vgontijo34@gmail.com>
2023-11-23 19:02:06 -0300

    SleepStress page created

---
+++ src/app/sleepStress/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import LogoHT from "../components/LogoHT";
+import Title from "../components/Title";
+import SleepStressForm from "../components/SleepStressForm";
+import { useState } from "react";
+import { sonoData } from "../types/types";
+import SleepStressResults from "../components/SleepStressResults";
+import LinkButton from "../components/LinkButton";
+import { PostSono } from "../api/apiUtils";
+import { useRouter } from "next/navigation";
+
+export default function SleepStress() {
+  const router = useRouter();
+  const [resultadoSono, setResultadoSono] = useState<sonoData | null>(null);
+
+  const handleRegister = async () => {
+    if (resultadoSono) {
+      await PostSono({ ...resultadoSono, id_cliente: 1 });
+      router.push("/sonos");
+    }
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24 pt-10 pr-10">
+      <div className="self-end">
+        <LogoHT />
+      </div>
+      <Title>{"SleepStress"}</Title>
+      {!resultadoSono ? (
+        <SleepStressForm setSonoData={setResultadoSono} />
+      ) : (
+        <div className="flex flex-col justify-center items-center">
+          <SleepStressResults sonoData={resultadoSono} />
+          <LinkButton onclick={handleRegister}>Registrar</LinkButton>
+        </div>
+      )}
+    </main>
+  );
+}
