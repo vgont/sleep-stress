37da52c4170383411f6b76686b6dde6474d14a03
vgont <vgontijo34@gmail.com>
2023-11-24 00:56:32 -0300

    postCliente added

--- src/app/api/apiUtils.ts
+++ src/app/api/apiUtils.ts
@@ -1,4 +1,4 @@
-import { sonoData } from "./../types/types";
+import { cliente, login, sonoData } from "./../types/types";
 import { AIFeatures, AITargets } from "../types/types";
 
 export async function qualificarSleepStress(data: AIFeatures) {
@@ -39,6 +39,58 @@ const qualidade_sono = (qualidade: number) => {
   if (qualidade >= 6) return "normal";
   return "ruim";
 };
+
+export async function PostCliente(clienteData: cliente) {
+  const apiUrl = "http://localhost:8080/cadastro";
+
+  const response = await fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(clienteData),
+  });
+  return response.json();
+}
+
+export async function GetClienteByUser(data: login) {
+  const apiUrl = "http://localhost:8080/validar";
+  const queryString = Object.entries(data)
+    .map(
+      ([key, value]) =>
+        `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+    )
+    .join("&");
+
+  const urlWithParams = `${apiUrl}?${queryString}`;
+
+  const response = await fetch(urlWithParams, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    const result: { success: boolean } = await response.json();
+    return result.success;
+  }
+  return { success: false, message: "An error occurred" };
+}
+
+export async function GetIdClienteByUser(user: string) {
+  const apiUrl = `http://localhost:8080/user/${user}`;
+
+  const response = await fetch(apiUrl);
+
+  if (response.ok) {
+    const result: { id_cliente: number; data_nasc: string } =
+      await response.json();
+    console.log(result);
+    return result;
+  } else {
+    console.log("Erro ao pegar id do cliente");
+  }
+}
 
 export async function PostSono(sonoData: sonoData) {
   const apiUrl = "http://localhost:8080/sono";
